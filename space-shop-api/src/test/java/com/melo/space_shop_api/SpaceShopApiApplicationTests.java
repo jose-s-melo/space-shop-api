119fa43b0ad124f441890c3da24304403cfd8937
José Melo <jos.ryan.melo@gmail.com>
2026-01-16 20:23:33 -0300

    Initial commit

---
+++ space-shop-api/src/test/java/com/melo/space_shop_api/SpaceShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melo.space_shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
