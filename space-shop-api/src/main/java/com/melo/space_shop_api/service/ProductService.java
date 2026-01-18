25d28aba32a4c9ee58efa150d4594c3d00456046
José Melo <jos.ryan.melo@gmail.com>
2026-01-18 09:27:41 -0300

    refactor: package structure

--- space-shop-api/src/main/java/com/melo/space_shop_api/service/ProductService.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/service/ProductService.java
@@ -2,8 +2,8 @@ package com.melo.space_shop_api.service;
 
 import org.springframework.stereotype.Service;
 
-import com.melo.space_shop_api.dto.AddProductDTO;
-import com.melo.space_shop_api.dto.ProductResponseDTO;
+import com.melo.space_shop_api.dto.product.AddProductDTO;
+import com.melo.space_shop_api.dto.product.ProductResponseDTO;
 import com.melo.space_shop_api.entity.Product;
 import com.melo.space_shop_api.exception.InvalidProductException;
 import com.melo.space_shop_api.repository.ProductRepository;
