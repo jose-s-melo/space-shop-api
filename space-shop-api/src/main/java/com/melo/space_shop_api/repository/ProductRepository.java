4db9a2e424f035b51fa9e36561c851df88e8150e
José Melo <jos.ryan.melo@gmail.com>
2026-01-18 10:39:14 -0300

    feat: new queries

--- space-shop-api/src/main/java/com/melo/space_shop_api/repository/ProductRepository.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.melo.space_shop_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.melo.space_shop_api.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    Optional<Product> findById(Long id);
+    void deleteById(Long id);
 }
