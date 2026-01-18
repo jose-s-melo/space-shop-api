47177c11e4f765f448f127a33058142d3309e1e1
José Melo <jos.ryan.melo@gmail.com>
2026-01-18 10:20:31 -0300

    feat: security config

--- space-shop-api/src/main/java/com/melo/space_shop_api/security/SecurityConfig.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/security/SecurityConfig.java
@@ -1,8 +1,21 @@
 package com.melo.space_shop_api.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
 }
