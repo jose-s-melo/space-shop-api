7dd264a416c34c5b2d8af0aa19d615e0b8f3732f
José Melo <jos.ryan.melo@gmail.com>
2026-01-17 21:09:51 -0300

    feat: add product in service

--- space-shop-api/src/main/java/com/melo/space_shop_api/service/ProductService.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/service/ProductService.java
@@ -2,7 +2,28 @@ package com.melo.space_shop_api.service;
 
 import org.springframework.stereotype.Service;
 
+import com.melo.space_shop_api.dto.AddProductDTO;
+import com.melo.space_shop_api.dto.ProductResponseDTO;
+import com.melo.space_shop_api.entity.Product;
+import com.melo.space_shop_api.repository.ProductRepository;
+
 @Service
 public class ProductService {
 
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public ProductResponseDTO addProduct(AddProductDTO dto) {
+        Product product = Product.builder()
+                .name(dto.name())
+                .price(dto.price())
+                .description(dto.description())
+                .build();
+
+        repository.save(product);
+        return new ProductResponseDTO(dto.name(), dto.price(),  dto.description());
+    }
 }
