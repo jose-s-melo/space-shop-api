acdee1970e9f375a422b29fc75ae644ab49ffa51
José Melo <jos.ryan.melo@gmail.com>
2026-01-18 10:20:44 -0300

    feat: authentication service

---
+++ space-shop-api/src/main/java/com/melo/space_shop_api/service/AuthenticationService.java
@@ -0,0 +1,24 @@
+package com.melo.space_shop_api.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.melo.space_shop_api.repository.UserRepository;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public AuthenticationService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username);
+    }
+
+}
