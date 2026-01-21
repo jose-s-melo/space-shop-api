a93dcd082309fa60ae0e4f2ef1b07a086f8ce4f7
José Melo <jos.ryan.melo@gmail.com>
2026-01-21 11:31:12 -0300

    fix: update authorization roles for product management and registration endpoints

--- space-shop-api/src/main/java/com/melo/space_shop_api/security/SecurityConfig.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/security/SecurityConfig.java
@@ -22,7 +22,9 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers(HttpMethod.POST, "/products").hasRole("ROLE_ADMIN")
+                    .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/auth/register").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
                     .anyRequest().authenticated()
                 )
                 .build();
