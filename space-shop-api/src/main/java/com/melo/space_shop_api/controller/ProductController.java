e82174319ce2b6b9abf373e0d884d8c3ee71bcda
José Melo <jos.ryan.melo@gmail.com>
2026-01-21 10:20:08 -0300

    feat(endpoint): update product

--- space-shop-api/src/main/java/com/melo/space_shop_api/controller/ProductController.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/controller/ProductController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.melo.space_shop_api.dto.product.AddProductDTO;
 import com.melo.space_shop_api.dto.product.ProductResponseDTO;
+import com.melo.space_shop_api.dto.product.UpdateProductDTO;
 import com.melo.space_shop_api.service.ProductService;
 
 @RestController
@@ -37,14 +39,21 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> addProduct(@RequestBody AddProductDTO body) {
+    public ResponseEntity<Void> addProduct(@Validated @RequestBody AddProductDTO body) {
         service.addProduct(body);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteProduct(@Validated @PathVariable Long id) {
         service.deleteProduct(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateProduct(@Validated @PathVariable Long id, 
+                                                @Validated @RequestBody UpdateProductDTO body) {
+        service.updateProduct(id, body);
         return ResponseEntity.ok().build();
     }
 }
