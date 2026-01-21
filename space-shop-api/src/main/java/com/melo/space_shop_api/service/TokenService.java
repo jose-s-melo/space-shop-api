94cb6c71667efdf9d0f107bd51d704d0dac784dc
José Melo <jos.ryan.melo@gmail.com>
2026-01-21 11:58:03 -0300

    feat: implement TokenService for JWT token generation

---
+++ space-shop-api/src/main/java/com/melo/space_shop_api/service/TokenService.java
@@ -0,0 +1,40 @@
+package com.melo.space_shop_api.service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.melo.space_shop_api.entity.User;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String SECRET;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET);
+            String token = JWT.create()
+                            .withIssuer("admin")
+                            .withSubject(user.getEmail())
+                            .withExpiresAt(generateExpirationDate())
+                            .sign(algorithm);
+            return token;
+        } catch (JWTCreationException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException("Error while generating token");
+        }
+    }
+
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
