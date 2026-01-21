c3b6e204bd41b3e5cc152347c7ae1b9130611d96
José Melo <jos.ryan.melo@gmail.com>
2026-01-21 12:19:04 -0300

    fix: add missing @Component annotation to SecurityFilter class

--- space-shop-api/src/main/java/com/melo/space_shop_api/security/SecurityFilter.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/security/SecurityFilter.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.melo.space_shop_api.repository.UserRepository;
@@ -16,6 +17,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
@@ -35,7 +37,7 @@ public class SecurityFilter extends OncePerRequestFilter {
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        
+
         filterChain.doFilter(request, response);
     }
 
