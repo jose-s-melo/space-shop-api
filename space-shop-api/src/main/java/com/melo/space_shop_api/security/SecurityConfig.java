5e5ab2d4fb88c000267c32f7ca8d05b5cf52e004
José Melo <jos.ryan.melo@gmail.com>
2026-01-21 10:55:04 -0300

    feat: authorize requests

--- space-shop-api/src/main/java/com/melo/space_shop_api/security/SecurityConfig.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.melo.space_shop_api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,10 @@ public class SecurityConfig {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers(HttpMethod.POST, "/products").hasRole("ROLE_ADMIN")
+                    .anyRequest().authenticated()
+                )
                 .build();
     }
 }
