93665ae95ef13a9e72da1df30dd9168a4235c3ac
José Melo <jos.ryan.melo@gmail.com>
2026-01-21 10:09:53 -0300

    feat(endpoint): get all products

--- space-shop-api/src/main/java/com/melo/space_shop_api/controller/ProductController.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/controller/ProductController.java
@@ -1,8 +1,27 @@
 package com.melo.space_shop_api.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.melo.space_shop_api.dto.product.ProductResponseDTO;
+import com.melo.space_shop_api.service.ProductService;
+
 @RestController
+@RequestMapping("products")
 public class ProductController {
 
+    private ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> getAll() {
+        return ResponseEntity.ok(service.getAllProducts());
+    }
 }
