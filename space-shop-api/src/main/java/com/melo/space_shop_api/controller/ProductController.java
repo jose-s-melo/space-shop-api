d1a7b815e55b9b54c14adb25f8720e358ca8d3e3
José Melo <jos.ryan.melo@gmail.com>
2026-01-21 10:17:05 -0300

    feat(endpoint): delete product

--- space-shop-api/src/main/java/com/melo/space_shop_api/controller/ProductController.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,12 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<Void> addProduct(@RequestBody AddProductDTO body) {
         service.addProduct(body);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        service.deleteProduct(id);
         return ResponseEntity.ok().build();
     }
 }
