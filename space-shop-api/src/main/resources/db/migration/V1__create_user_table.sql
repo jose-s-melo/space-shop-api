73e57a7ef410fd3418e2434abdb1ff55c43a8bb0
José Melo <jos.ryan.melo@gmail.com>
2026-02-05 09:14:06 -0300

    feat(db): users table

---
+++ space-shop-api/src/main/resources/db/migration/V1__create_user_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+  id BIGSERIAL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  role VARCHAR(50) NOT NULL,
+
+  CONSTRAINT pk_users PRIMARY KEY (id),
+  CONSTRAINT uk_users_email UNIQUE (email),
+  CONSTRAINT chk_users_role CHECK (role IN ('ADMIN', 'USER'))
+);
