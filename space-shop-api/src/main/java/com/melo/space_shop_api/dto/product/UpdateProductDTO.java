2c8d22bb23f518e48a5c4781bc4e294115831377
José Melo <jos.ryan.melo@gmail.com>
2026-02-08 16:09:38 -0300

    feat: add id to update product dto

--- space-shop-api/src/main/java/com/melo/space_shop_api/dto/product/UpdateProductDTO.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/dto/product/UpdateProductDTO.java
@@ -3,6 +3,7 @@ package com.melo.space_shop_api.dto.product;
 import java.math.BigDecimal;
 
 public record UpdateProductDTO(
+    Long id,
     String name,
     BigDecimal price,
     String description
