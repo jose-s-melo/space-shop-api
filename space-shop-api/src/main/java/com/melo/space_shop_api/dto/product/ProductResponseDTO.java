ee1082d53bd3fbdfa6d4575826fea536bc377cee
José Melo <jos.ryan.melo@gmail.com>
2026-02-08 15:08:59 -0300

    feat: add id to response dto

--- space-shop-api/src/main/java/com/melo/space_shop_api/dto/product/ProductResponseDTO.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/dto/product/ProductResponseDTO.java
@@ -2,6 +2,6 @@ package com.melo.space_shop_api.dto.product;
 
 import java.math.BigDecimal;
 
-public record ProductResponseDTO(String name, BigDecimal price, String description) {
+public record ProductResponseDTO(Long id, String name, BigDecimal price, String description) {
 
 }
