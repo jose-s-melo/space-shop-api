8f53bc9ebbbd0ca2b1ed46831b5bff4a01a2ade3
José Melo <jos.ryan.melo@gmail.com>
2026-02-09 13:10:36 -0300

    fix: add stock to dtos

--- space-shop-api/src/main/java/com/melo/space_shop_api/dto/product/UpdateProductDTO.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/dto/product/UpdateProductDTO.java
@@ -1,10 +1,12 @@
 package com.melo.space_shop_api.dto.product;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 public record UpdateProductDTO(
     Long id,
     String name,
     BigDecimal price,
-    String description
-) {}
+    String description,
+    Integer stock
+) implements Serializable {}
