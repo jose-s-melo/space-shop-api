d354d5cbddc0bb819efa5e74af868c45465c5cbb
José Melo <jos.ryan.melo@gmail.com>
2026-02-11 15:13:28 -0300

    feat(redis): add item to cart

---
+++ space-shop-api/src/main/java/com/melo/space_shop_api/repository/CartRepository.java
@@ -0,0 +1,26 @@
+package com.melo.space_shop_api.repository;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class CartRepository {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    private String buildKey(Long userId) {
+        return "cart:" + userId;
+    }
+
+    public void addItem(Long userId, Long productId, Integer quantity) {
+        String key = buildKey(userId);
+        redisTemplate.opsForHash().increment(key, productId.toString(), quantity);
+        redisTemplate.expire(key, Duration.ofHours(24));
+    }
+
+}
