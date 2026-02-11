b1158de1043fbe7cc79dcc22239f990b2223bfdd
José Melo <jos.ryan.melo@gmail.com>
2026-02-11 14:26:09 -0300

    feat: add cart item

---
+++ space-shop-api/src/main/java/com/melo/space_shop_api/entity/CartItem.java
@@ -0,0 +1,33 @@
+package com.melo.space_shop_api.entity;
+
+public class CartItem {
+
+    private Product product;
+    private Integer quantity;
+
+    public CartItem() {
+    }
+
+    public CartItem(Product product, Integer quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    
+}
