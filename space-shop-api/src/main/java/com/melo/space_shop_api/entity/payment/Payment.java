d104dd1387deeb099ad7fb32e30dcc5899190eab
José Melo <jos.ryan.melo@gmail.com>
2026-02-15 14:23:13 -0300

    feat: payment entity

---
+++ space-shop-api/src/main/java/com/melo/space_shop_api/entity/payment/Payment.java
@@ -0,0 +1,96 @@
+package com.melo.space_shop_api.entity.payment;
+
+import java.math.BigDecimal;
+
+import com.melo.space_shop_api.entity.user.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Positive;
+
+@Entity
+@Table(name="payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Positive
+    @Column(nullable=false)
+    private BigDecimal value;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod method;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    public Payment(Long id, @Positive BigDecimal value, PaymentMethod method, PaymentStatus status, User user) {
+        this.id = id;
+        this.value = value;
+        this.method = method;
+        this.status = status;
+        this.user = user;
+    }
+
+    public Payment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public PaymentMethod getMethod() {
+        return method;
+    }
+
+    public void setMethod(PaymentMethod method) {
+        this.method = method;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    
+
+
+}
