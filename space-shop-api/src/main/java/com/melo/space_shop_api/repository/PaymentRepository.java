80bcac665850431b3df559674b7fc720d94c21d4
José Melo <jos.ryan.melo@gmail.com>
2026-02-17 11:42:39 -0300

    feat: query to find payment by order

--- space-shop-api/src/main/java/com/melo/space_shop_api/repository/PaymentRepository.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/repository/PaymentRepository.java
@@ -1,9 +1,12 @@
 package com.melo.space_shop_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.melo.space_shop_api.entity.payment.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
+    public Optional<Payment> findByOrderId(Long id);
 }
