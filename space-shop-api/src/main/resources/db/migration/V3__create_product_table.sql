353744c3d9d914436f4ca1aac319e320b75b2f96
José Melo <jos.ryan.melo@gmail.com>
2026-02-18 16:17:00 -0300

    feat(db): add sku of product

--- space-shop-api/src/main/resources/db/migration/V3__create_product_table.sql
+++ space-shop-api/src/main/resources/db/migration/V3__create_product_table.sql
@@ -4,8 +4,10 @@ CREATE TABLE products (
     description VARCHAR(500) NOT NULL,
     price NUMERIC(10, 2) NOT NULL,
     stock INTEGER NOT NULL,
+    sku VARCHAR(150) NOT NULL,
 
     CONSTRAINT pk_products PRIMARY KEY (id),
     CONSTRAINT chk_price CHECK (price >= 0),
-    CONSTRAINT chk_stock CHECK (stock >= 0)
+    CONSTRAINT chk_stock CHECK (stock >= 0),
+    CONSTRAINT uk_sku UNIQUE (sku)
 );
