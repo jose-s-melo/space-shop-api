ce056c68e739b09c0476c5dcfb021b62ff558fb0
José Melo <jos.ryan.melo@gmail.com>
2026-02-18 16:16:39 -0300

    feat(db): add description atribute

--- space-shop-api/src/main/resources/db/migration/V2__create_category_table.sql
+++ space-shop-api/src/main/resources/db/migration/V2__create_category_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE categories (
     id BIGSERIAL,
-
     name VARCHAR(50) NOT NULL,
+    description VARCHAR(512),
 
     CONSTRAINT pk_categories PRIMARY KEY (id),
     CONSTRAINT uk_name_categories UNIQUE (name)
