972a1fe1912e6f2b5bc5dceb0910edaa7baca69a
José Melo <jos.ryan.melo@gmail.com>
2026-02-25 14:18:59 -0300

    feat: change request role

--- space-shop-api/src/main/java/com/melo/space_shop_api/security/SecurityConfig.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/auth/register").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                     .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
                     .requestMatchers(HttpMethod.POST, "/orders/send").hasRole("ADMIN")
                     .anyRequest().authenticated()
