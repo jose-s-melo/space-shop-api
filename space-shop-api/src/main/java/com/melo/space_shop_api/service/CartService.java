adb1f8a5567b9d7a7f23dd44741549f7c3fb25f7
José Melo <jos.ryan.melo@gmail.com>
2026-03-21 17:36:02 -0300

    fix: check if a product exists before add

--- space-shop-api/src/main/java/com/melo/space_shop_api/service/CartService.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/service/CartService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.melo.space_shop_api.entity.user.User;
+import com.melo.space_shop_api.exception.ProductNotFoundException;
+import com.melo.space_shop_api.repository.ProductRepository;
 import com.melo.space_shop_api.repository.redis.CartRepository;
 
 @Service
@@ -15,12 +17,19 @@ public class CartService {
     private CartRepository cartRepository;
 
     @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
     private AuthenticationService authenticationService;
 
     public void addProduct(Long productId, Integer quantity) {
         User user = authenticationService.getCurrentUser();
 
         if (user != null && productId != null) {
+            if (!productRepository.existsById(productId)) {
+                throw new ProductNotFoundException();
+            }
+
             if (quantity != null && quantity.compareTo(0) > 0) {
                 cartRepository.addItem(user.getId(), productId, quantity);
             } else {
