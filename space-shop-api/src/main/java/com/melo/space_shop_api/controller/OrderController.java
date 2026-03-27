7b9ee005dd685d14bf06e71c6912c95755045abd
José Melo <jos.ryan.melo@gmail.com>
2026-03-27 20:21:26 -0300

    feat: add endpoint to obtain delivered, get total and an order

--- space-shop-api/src/main/java/com/melo/space_shop_api/controller/OrderController.java
+++ space-shop-api/src/main/java/com/melo/space_shop_api/controller/OrderController.java
@@ -1,16 +1,22 @@
 package com.melo.space_shop_api.controller;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.melo.space_shop_api.dto.order.OrderDetailResponseDTO;
 import com.melo.space_shop_api.dto.order.OrderResponseDTO;
 import com.melo.space_shop_api.dto.payment.PaymentMessageDTO;
 import com.melo.space_shop_api.service.OrderService;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 
 @RestController
@@ -41,4 +47,21 @@ public class OrderController {
         orderService.sendOrder(id);
         return ResponseEntity.ok().build();
     }
+
+    @PostMapping("/{id}/delivered")
+    public ResponseEntity<Void> deliveredOrder(@PathVariable Long id, @RequestBody Instant date) {
+        orderService.deliveredAt(id, date);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}/total")
+    public ResponseEntity<BigDecimal> getTotal(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.getTotal(id));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDetailResponseDTO> getOrder(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.getOrder(id));
+    }
+
 }
